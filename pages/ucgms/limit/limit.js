6d5098ebd88966e0d671c54139a7f840aec2268e
qpsandy <qpsandy@126.com>
2017-02-08 23:41:53 +0800

    limit page css

--- pages/ucgms/limit/limit.js
+++ pages/ucgms/limit/limit.js
@@ -1,12 +1,81 @@
 Page({
   data: {
+    orgSheetHidden: true,
+    lineSheetHidden: true,
     customerId: 0,
     customerName: 0,
-    organization: ['中国', '美国', '巴西', '日本'],
-    line: ['企金金融','金融市场','零售业务'],
-    orgindex: 0,
-    lineindex: 0,
-    busiType:0
+    custCodeHolder: '请填写客户代码',
+    custNameHolder: '请填写客户名称',
+    organization: '请选择经办机构',
+    line: '请选择业务条线',
+    organizations: [
+    {
+      _id: '1',
+      name:'中国'
+    },
+    {
+      _id: '2',
+      name: '巴西'
+    },
+    {
+      _id: '3',
+      name: '日本'
+    }],
+    lines: [
+    {
+      _id: '1',
+      name:'企金金融'
+    },
+    {
+      _id: '2',
+      name: '金融市场'
+    },
+    {
+      _id: '3',
+      name: '零售业务'
+    }]
+  },
+  orgSheetChange: function(e) {
+    this.setData({
+      orgSheetHidden: !this.data.orgSheetHidden
+    })
+  },
+  orgSheetTap: function(e) {
+    console.log(e);
+    this.setData({
+      orgSheetHidden: !this.data.orgSheetHidden
+    })
+  },
+  orgSheetItemTap: function(e) {
+    var nameAndId=e.currentTarget.id;
+    var strs= new Array(); //定义一数组 
+    strs=nameAndId.split(","); //字符分割 
+    this.setData({
+      orgSheetHidden: true,
+      organizationId:strs[0],
+      organization:strs[1]
+    })
+  },
+  lineSheetChange: function(e) {
+    this.setData({
+      lineSheetHidden: !this.data.lineSheetHidden
+    })
+  },
+  lineSheetTap: function(e) {
+    console.log(e);
+    this.setData({
+      lineSheetHidden: !this.data.lineSheetHidden
+    })
+  },
+  lineSheetItemTap: function(e) {
+    var nameAndId=e.currentTarget.id;
+    var strs= new Array(); //定义一数组 
+    strs=nameAndId.split(","); //字符分割 
+    this.setData({
+      lineSheetHidden: true,
+      lineId:strs[0],
+      line:strs[1]
+    })
   },
   bindCustomerIdChange: function(e) {
     console.log('customerId发送输入，携带值为', e.detail.value)
